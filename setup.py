14914219aa466d72477716cf246cc73020d8127b
arundasan91 <ard212@pitt.edu>
2024-11-29 21:20:36 -0500

    update license in setup.py

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+	'License :: OSI Approved :: Apache Software License',
+	'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
